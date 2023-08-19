650465a35a79869372cab08b19513968f50ab88b
Anirudh Madhav Kulkarni <Anirudhkulkarni9094@gmail.com>
2023-08-19 12:57:45 +0530

    added new router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './Component/HomePage/Home';
 import Blog from './Component/My_Blog/Blog';
 import NavBar from './Component/NavBar/NavBar';
@@ -9,18 +9,16 @@ import FeedBack from './Component/FeedBack/FeedBack';
 
 function App() {
   return (
-    <Router>
-      <>
-        <NavBar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/My-Blog" component={Blog} />
-          <Route path="/My-Project" component={Project} />
-          <Route path="/About-me" component={About} />
-          <Route path="/Feedback" component={FeedBack} />
-        </Switch>
-      </>
-    </Router>
+    <Routes>
+      <Route path="/" element={<NavBar />}>
+        <Route index element={<Home />} />
+        <Route path="blog" element={<Blog />} />
+        <Route path="projects" element={<Project />} />
+        <Route path="about" element={<About />} />
+        <Route path="feedback" element={<FeedBack />} />
+        <Route path="*" render={() => <Navigate to="/" />} />
+      </Route>
+    </Routes>
   );
 }
 
