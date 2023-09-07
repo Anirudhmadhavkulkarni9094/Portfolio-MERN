70c6db2b0f1dbc79f73a9017c260f69af172ec13
Anirudh Madhav Kulkarni <Anirudhkulkarni9094@gmail.com>
2023-09-07 15:26:28 +0530

    fixing bugs

--- server/server.js
+++ server/server.js
@@ -14,99 +14,117 @@ app.use(body_parser.json());
 
 
 
-mongoose.connect('mongodb+srv://anirudhkulkarni9094:yj7Hp6RBnEEf0aQV@cluster0.13ujdku.mongodb.net/?retryWrites=true&w=majority').then(res=>{
-    console.log("connection established successfully");
-}).catch(err => {
-    console.log("cannot connect to database " + err )
-} )
+mongoose.connect('mongodb+srv://anirudhkulkarni9094:yj7Hp6RBnEEf0aQV@cluster0.13ujdku.mongodb.net/?retryWrites=true&w=majority' , { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+    console.log("Connection to MongoDB established successfully");
+})
+.catch(err => {
+    console.error("Cannot connect to MongoDB:", err);
+});
 
+// Define routes
 
+// Add a new blog
 app.post('/add-blogs', addBlogs);
-app.delete('/delete-blog/:id' , DeleteBlog)
 
+// Delete a blog by ID
+app.delete('/delete-blog/:id', DeleteBlog);
 
+// Get all blogs
 app.get('/get-blogs', async (req, res) => {
-    try {
-        const BlogsData = await blog.find({});
-        // console.log(BlogsData);
-        res.status(200).json({
-            message: "Blogs fetched successfully",
-            data: BlogsData
-        });
-    } catch (err) {
-        // console.error(err);
-        res.status(500).json({
-            message: "Failed to fetch blogs",
-            error: err.message
-        });
-    }
+try {
+    const BlogsData = await blog.find({});
+    res.status(200).json({
+        message: "Blogs fetched successfully",
+        data: BlogsData
+    });
+} catch (err) {
+    console.error("Failed to fetch blogs:", err.message);
+    res.status(500).json({
+        message: "Failed to fetch blogs",
+        error: err.message
+    });
+}
 });
 
+// Add a new project
 app.post('/add-project', async (req, res) => {
-    try {
-        const newProject = new Project(req.body);
-        await newProject.save();
-        console.log("Project added successfully");
-        console.log("Data added successfully");
-        res.status(200).json({
-            message: "Data added successfully"
-        });
-    } catch (err) {
-        console.error("Data cannot be added:", err.message);
-        res.status(500).json({
-            message: "Failed to add data",
-            error: err.message
-        });
-    }
+try {
+    const newProject = new Project(req.body);
+    await newProject.save();
+    console.log("Project added successfully");
+    res.status(200).json({
+        message: "Project added successfully"
+    });
+} catch (err) {
+    console.error("Failed to add project:", err.message);
+    res.status(500).json({
+        message: "Failed to add project",
+        error: err.message
+    });
+}
 });
 
-
-
-app.get('/get-projects' , async (req, res)=>{
-    try{
-        const projectList  = await Project.find();
-        res.status(201).json({
-            message : "Data fetched successfully",
-            data : projectList
-        }) 
-    }
-    catch{
-        res.status(201).json({
-            message : "data cannot be fetched"
-        })
-    }
-})
-
-app.get('/feedback' , async (req, res)=>{
-    try{
-        const feedbacks = await Feedback.find();
-        res.status(200).json({
-            message : "Feedback fetched",
-            data : feedbacks
-        })
-    }
-    catch(err){
-        res.status(400).json({
-            message : "feedback cannot be fetched"
-        })
-    }
-})
-
-app.post('/add-feedback' , async (req, res) => {
-    try {
-        await Feedback.create(req.body); // Use 'await' to wait for the data to be added
-        res.status(201).json({
-            message: "Feedback added successfully"
-        });
-    } catch (err) {
-        res.status(400).json({
-            message: "Feedback could not be added"
-        });
-    }
+// Get all projects
+app.get('/get-projects', async (req, res) => {
+try {
+    const projectList = await Project.find();
+    res.status(200).json({
+        message: "Data fetched successfully",
+        data: projectList
+    });
+} catch (err) {
+    console.error("Failed to fetch projects:", err.message);
+    res.status(500).json({
+        message: "Failed to fetch projects",
+        error: err.message
+    });
+}
 });
 
+// Get all feedback
+app.get('/feedback', async (req, res) => {
+try {
+    const feedbacks = await Feedback.find();
+    res.status(200).json({
+        message: "Feedback fetched",
+        data: feedbacks
+    });
+} catch (err) {
+    console.error("Failed to fetch feedback:", err.message);
+    res.status(500).json({
+        message: "Failed to fetch feedback",
+        error: err.message
+    });
+}
+});
 
+// Add new feedback
+app.post('/add-feedback', async (req, res) => {
+try {
+    await Feedback.create(req.body);
+    res.status(201).json({
+        message: "Feedback added successfully"
+    });
+} catch (err) {
+    console.error("Failed to add feedback:", err.message);
+    res.status(500).json({
+        message: "Failed to add feedback",
+        error: err.message
+    });
+}
+});
 
-app.listen(3001,()=>{
-    console.log("server running at port 3000");
-})
+// Start the server
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+console.log(`Server is running on port ${port}`);
+});
+
+// Graceful shutdown
+process.on('SIGINT', () => {
+mongoose.connection.close(() => {
+    console.log('Mongoose connection is disconnected due to app termination');
+    process.exit(0);
+});
+});
