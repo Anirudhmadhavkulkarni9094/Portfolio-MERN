409536bdfd2c805a741405a66881a46d44b6fc2a
Anirudhmadhavkulkarni9094 <Anirudhkulkarni9094@gmail.com>
2023-09-16 01:03:49 +0530

    changed layout

--- server/server.js
+++ server/server.js
@@ -53,7 +53,7 @@ try {
     const newProject = new Project(req.body);
     await newProject.save();
     console.log("Project added successfully");
-    res.status(200).json({
+    res.status(201).json({
         message: "Project added successfully"
     });
 } catch (err) {
@@ -81,6 +81,33 @@ try {
     });
 }
 });
+app.delete('/project/:id' , async (req, res) => {
+    const blogId = req.params.id;
+    try {
+        // Find the blog post by ID and remove it from the database
+        const deletedBlog = await Project.findByIdAndRemove(blogId);
+
+        if (!deletedBlog) {
+            // Blog post with the provided ID not found
+            return res.status(404).json({
+                message: 'Blog post not found',
+            });
+        }
+
+        // Blog post successfully deleted
+        res.status(200).json({
+            message: 'Blog post deleted successfully',
+            data: deletedBlog,
+        });
+    } catch (error) {
+        // Handle any errors that occurred during the deletion process
+        console.error('Error deleting blog post:', error);
+        return res.status(500).json({
+            message: 'Failed to delete blog post',
+            error: error.message,
+        });
+    }
+})
 
 // Get all feedback
 app.get('/feedback', async (req, res) => {
